da54474cc962bc38953f6ce3839e1df1ff82b053
Julian Thanner <julian.thanner@gmx.de>
2023-10-14 14:19:09 +0200

    .

--- index.js
+++ index.js
@@ -3,15 +3,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'admin',
+    host: 'localhost',
+    database: 'check24',
+    password: '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',
+    port: 9856,
+});
+
+client.connect();
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index', {time: new Date()});
-
 })
 
 app.get('/search', (req, res) => {
-   res.render('search', {q: req.query})
+    client.query('SELECT * FROM products', (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(res.rows);
+
+        res.render('search', {table: res.rows});
+    });
 })
 
 app.listen(port, () => {

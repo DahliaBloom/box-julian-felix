ba16614fb7067b01a5f79b21c2fc2564c5228fb0
Julian Thanner <julian.thanner@gmx.de>
2023-10-14 15:50:50 +0200

    added sql escape

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ app.get('/search', (req, res) => {
         });
 
     } else {
-        client.query(`SELECT * FROM products WHERE name like \'%${req.query.q}%\'`, (err, ress) => {
+
+
+        client.query(`SELECT * FROM products WHERE name like \'%${mysql_real_escape_string(req.query.q)}%\'`, (err, ress) => {
             if (err) {
                 console.error(err);
                 return;
@@ -57,3 +59,35 @@ generateDB.run(client);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+function mysql_real_escape_string (str) {
+    return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
+        switch (char) {
+            case "\0":
+                return "\\0";
+            case "\x08":
+                return "\\b";
+            case "\x09":
+                return "\\t";
+            case "\x1a":
+                return "\\z";
+            case "\n":
+                return "\\n";
+            case "\r":
+                return "\\r";
+            case "\"":
+            case "'":
+                return "''"; // prepends a backslash to backslash, percent,
+
+            case "\\":
+            case "%":
+                return "\\"+char; // prepends a backslash to backslash, percent,
+                                  // and double/single quotes
+            default:
+                return char;
+        }
+    });
+}
